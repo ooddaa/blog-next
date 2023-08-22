a97493e85a0d35f0a1967bf068658987cc16b485
oda <ooddaa@gmail.com>
2023-08-22 12:24:45 +0100

    [welcome] footer in mobile, bio text
    CHANGES:
    0. placed footer in mobile verion correctly
    1. changed bio text
    2. removed tags margins to align the box
    
    TODO:
    0. click tag -> go to filtered blog
    1. add portfolio page

--- app/page.tsx
+++ app/page.tsx
@@ -15,20 +15,21 @@ export default function Welcome(): JSX.Element {
   }, [])
 
   return (
-    <div className={`welcome w-screen h-screen relative`}>
+    <div className={`welcome w-screen h-screen relative bg-[#f9fafc] -z-20`}>
       {innerWidth > 400 ? <div className="welcome--img bg-top absolute top-[156px] left-0 w-full h-[calc(100%-156px)] -z-10"></div> : ""}
       
-      <section className="w-full h-full lg:h-screen flex flex-col lg:flex-row">
+      <div className="w-full sm:h-screen flex flex-col lg:flex-row">
+      {/* <div className="w-full flex flex-col lg:flex-row"> */}
         
         {/* left */}
-        <section className="h-[300px] pt-12 lg:pt-48 lg:h-full w-full lg:w-1/2 bg-green-800/50 flex flex-col justify-center items-center lg:justify-start text-slate-900">
+        <div className="h-[300px] pt-12 lg:pt-48 lg:h-full w-full lg:w-1/2 bg-green-800/50 flex flex-col justify-center items-center lg:justify-start text-slate-900">
             <div
                 className="max-sm:min-h-[128px] pt-24 text-[36px] lg:text-[64px] font-bold lg:font-semibold flex flex-row justify-center items-center text-center"
             > 
               {innerWidth <= 400 ? "Dmitry Vikhorev" : "Dmitry 'Oda' Vikhorev"}
             </div>
 
-              <div className="text-[18px] max-sm:pb-12 lg:pt-24 font-medium flex flex-col justify-center items-center gap-4 relative">
+            <div className="text-[18px] max-sm:py-12 lg:pt-24 flex flex-col justify-center items-center gap-4 relative">
                 <div className="navbar-links">
                   <Link href="/blog"> blog </Link>
                   
@@ -45,52 +46,56 @@ export default function Welcome(): JSX.Element {
                   <Link href="/playground">playground</Link>
                 </div>
             </div>
-        </section>
+        </div>
 
         {/* right */}
-        <section className="w-full lg:w-1/2 flex flex-col justify-start items-center p-12">
+        <div className="w-full lg:w-1/2 flex flex-col justify-start items-center p-12">
             <Text size="lg">
               <div className="pb-4">
-                Over the past decade, I worked as:
+                {/* I worked as: */}
                 <ul className="pt-4">
-                  <li><GradientSpan from="green" to="cyan">Business Developer</GradientSpan></li>
-                  <li><GradientSpan from="red" to="orange">Software Engineer</GradientSpan></li>
-                  <li><GradientSpan from="cyan" to="green">Full time dad</GradientSpan></li>
-                  <li><GradientSpan from="cyan" to="blue">English teacher</GradientSpan></li>
-                  <li><GradientSpan from="Purple" to="blue">Entrepreneur</GradientSpan></li>
+                  <li><GradientSpan from="red" to="orange">Business Developer</GradientSpan></li>
+                  <li><GradientSpan from="orange" to="yellow">Software Engineer</GradientSpan></li>
+                  <li><GradientSpan from="yellow" to="green">Full time dad</GradientSpan></li>
+                  <li><GradientSpan from="green" to="blue">English teacher</GradientSpan></li>
+                  <li><GradientSpan from="blue" to="purple">Entrepreneur</GradientSpan></li>
                 </ul>
               </div>
               <div className="pb-4">
-                I get a knack out of keeping records and writing things down.
+                I get a knack out of finding out how things work, usually by breaking them up 😂.
               </div>
               <div className="pb-4">
-              As the result I have accumulated a bunch of designs, notes, projects, code, googlesheets and business ideas. I would like to keep playing with some of them and learn more. 
+                whilst keeping records and writing things down so I don&apos;t forget.
+              </div>
+              {/* <div className="pb-4">
+              I have a bunch of designs, notes, projects, code, googlesheets and business ideas. I would like to keep playing with them and learn more. 
+              </div> */}
+              <div className="pb-4">
+              if you find anything interesting here, send an <a className="underline text-blue-500" href="mailto:ooddaa@gmail.com">email</a> or message me on <a className="underline text-blue-500" href="https://discordapp.com/users/774603540278673419">Discord</a>.
               </div>
               <div className="pb-4">
-              If you find anything interesting here, I&apos;d love to <a className="underline text-blue-500" href="mailto:ooddaa@gmail.com">talk</a>! 
-              </div>
-              <div className="pb-4">
-              This is my sandbox, everyone&lsquo;s welcome, BYOT
+              this is my sandbox, everyone&lsquo;s welcome, BYOT
               </div>
               <div className="mb-8">
-                I also like <a className="underline text-purple-950" href="https://rogergracie.com/">BJJ</a>.
+                I also like <a className="underline text-purple-950" href="https://rogergracie.com/">BJJ</a> and think that capital letters are overrated.
               </div>
             </Text>
-            <section className="my-toys w-full">
-              <h2 className="font-semibold pb-4 text-lg">Stack that I&apos;m using daily:</h2>
-              <ul className="flex flex-row flex-wrap">
-                {["TypeScript", "React", "Next.js", "Elixir", "Phoenix", "Phoenix LiveView", "Figma"]
-                 .map(val => <Tag tag={val} key={val} classNames={"p-2 pl-3 pr-3 m-2 h-auto w-max rounded-md text-sm transition delay-50 select-none hover:cursor-pointer shadow-md"}/>)}
+            <div className="my-toys w-full">
+              {/* <h2 className="font-semibold pb-4 text-lg">Stack that I&apos;m using daily:</h2> */}
+              <ul className="flex flex-row flex-wrap gap-2">
+                {["TypeScript", "React", "Next.js", "Elixir", "Phoenix", "Figma", "Phoenix LiveView", ]
+                 .map(val => <Tag tag={val} key={val} classNames={"p-2 pl-3 pr-3 h-auto w-max rounded-md text-sm transition delay-50 select-none hover:cursor-pointer shadow-md"}/>)}
                 
               </ul>  
-            </section>
-        </section>
-      </section>
+            </div>
+        </div>
+      </div>
 
-      <div className="bg-transparent h-[16px] px-4 my-4 absolute bottom-0">
+      <div className="bg-transparent h-[16px] px-4 my-4 max-sm:py-4 sm:absolute sm:bottom-0">
+      {/* <div className="bg-transparent px-4"> */}
         <p className="text-gray-500 text-sm"> 
           made with 👾 by <span className="oda text-amber-500">oda</span> | photo by{" "}
-          <a href="https://unsplash.com/photos/cBhk90BuOeU">Greg Torosiants</a>
+          Greg Torosiants
         </p>
       </div>
     </div>

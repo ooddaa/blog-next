d7c518c14c30ed3e91ac630a63dd1199ec4e1219
oda <ooddaa@gmail.com>
2023-11-10 16:41:44 +0000

    [Blog] move code highlight css

--- pages/blog/components/PostLayout.tsx
+++ pages/blog/components/PostLayout.tsx
@@ -10,28 +10,6 @@ export default function PostLayout({ children }: { children: React.ReactNode }) 
     <>
     <MantineHeader />
       <div className={["wrapper", font.className, "w-full max-md:px-4 md:w-3/5 pb-8 mx-auto text-base/6 md:text-lg/8"].join(" ")}>{children}</div>
-      <style jsx global>{`
-        code {
-          font-family: 'Menlo';
-          font-size: 14px;
-          line-height: 20px;
-        }
-        span > code > span > span {
-          font-family: 'Menlo';
-          font-size: 14px;
-          line-height: 20px;
-          padding: 4px;
-          background-color: #f9fafb;
-          color: #1e293b;
-        }
-        pre {
-          border-radius: 8px;
-          padding: 16px;
-        }
-        [data-highlighted-chars] {
-          background-color: grey;
-        }
-      `}</style>
     </>
   )
 }
